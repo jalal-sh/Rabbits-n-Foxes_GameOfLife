d32e6ae6b44a78ad2926473e90c5845e72c4728c
jalal.syria <jalal.syria@gmail.com>
2015-08-14 17:31:07 +0300

    No Generations

---
+++ Rabbits'n'Foxes_GameOfLife/Rabbit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbits_n_Foxes_GameOfLife
+{
+    public class Rabbit
+    {
+        /// <summary>
+        /// Age of The Rabbit (Represented as Days)
+        /// </summary>
+        public int Age { get; private set; }
+        
+    }
+}
